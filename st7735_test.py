ff4ca0afa6107652ff977e5bd1c6c8768a302042
Ivan Lipatov <moon.diller@gmail.com>
2024-07-10 15:12:29 +0200

    Upd: snake widget was added

--- st7735_test.py
+++ st7735_test.py
@@ -450,6 +450,96 @@ class TextLabel(RectWidget):
         self._need_redraw = True
 
 
+import termios, fcntl, sys, os
+class SnakeWidget(RectWidget):
+    def __init__(
+    self,
+    lcd: LcdWrapper,
+    parent: RectWidget,
+    relx: int,
+    rely: int,
+    width: int,
+    height: int,
+    bg_color: int,
+    ):
+        super().__init__(lcd, parent, relx, rely, width, height, bg_color)
+        self.dxdy = (1, 0)
+        self.posx = (self.x0 + self.x1) // 2
+        self.posy = (self.y0 + self.y1) // 2
+
+        # init pos
+        cur_len = 3
+        assert(cur_len > (self.y1 - self.y1))
+        
+        self.snake = []
+        for i in range(cur_len):
+            self.snake.append((self.posx - i, self.posy))
+            self._lcd.pixel(self.posx - i, self.posy, color=self._lcd._COLOR_MAGENTA)
+
+        self.head = cur_len - 1
+        self.tail = 0
+
+
+    def draw(self):
+        # color old tail
+        tail_pixel_coord = self._rel_to_abs_point(self.snake[self.tail][0], self.snake[self.tail][1])
+        self._lcd.pixel(tail_pixel_coord[0], tail_pixel_coord[1], color=self.color)
+
+        # upd dxdy and length
+        key = self._getkey()
+        if key in [65, 66, 67, 68, 32]:
+            status_led.value = 1
+        else:
+            status_led.value = 0
+
+        if key == 67:
+            self.dxdy = (-1, 0)
+        if key == 68:
+            self.dxdy = (1, 0)
+        if key == 65:
+            self.dxdy = (0, 1)
+        if key == 66:
+            self.dxdy = (0, -1)
+        if key == 32:
+            self.snake.append(self.snake[-1])
+
+        new_head_x = (self.snake[self.head][0] + self.dxdy[0]) % (self.x1 - self.x0)
+        new_head_y = (self.snake[self.head][1] + self.dxdy[1]) % (self.y1 - self.y0)
+        self.head = (self.head + 1) % len(self.snake)
+        self.tail = (self.tail + 1) % len(self.snake)
+        self.snake[self.head] = (new_head_x, new_head_y)
+
+        # color new head
+        head_pixel_coord = self._rel_to_abs_point(self.snake[self.head][0], self.snake[self.head][1])
+        self._lcd.pixel(head_pixel_coord[0], head_pixel_coord[1], color=self._lcd._COLOR_MAGENTA)
+    
+    def _rel_to_abs_point(self, relx, rely):
+        return relx + self.x0, rely + self.y0
+            
+    
+    def _getkey(self):
+        fd = sys.stdin.fileno()
+
+        oldterm = termios.tcgetattr(fd)
+        newattr = termios.tcgetattr(fd)
+        newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+        termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+        oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+        fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+
+        c = None
+
+        try:
+            c = sys.stdin.read(1)
+        except IOError: pass
+
+        termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+        fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+        # print("got key:", c, ord(c))
+        return ord(c) if c else None
+
+
 
 if __name__ == "__main__":
     import time
@@ -487,12 +577,14 @@ if __name__ == "__main__":
 
         main_widget = RectWidget(lcd, None, 0, 0, lcd.width, lcd.height, lcd._COLOR_BLACK)
         date_widget = TextLabel(lcd, main_widget, 0, 0, lcd.width, 10, font_color=0xFFFFFF, bg_color=lcd._COLOR_GREEN)
-        text_widget = TextLabel(lcd, main_widget, 0, 145, lcd.width, 10, font_color=0xFFFFFF, bg_color=lcd._COLOR_GREEN, text="Hello, world!")
+        text_widget = TextLabel(lcd, main_widget, 0, 150, lcd.width, 10, font_color=0xFFFFFF, bg_color=lcd._COLOR_GREEN, text="Hello, world!")
+        snake_widget = SnakeWidget(lcd, main_widget, 0, 10, lcd.width, 140, bg_color=lcd._COLOR_BLACK)
         
         while True:
             date_widget.text = time.ctime()
+            snake_widget._need_redraw = True
             main_widget.draw()
-            time.sleep(0.2)
+            time.sleep(0.05)
 
 
         status_led.value = 0
